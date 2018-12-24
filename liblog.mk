ad38b3220229e2abff87ad2c5f1d1d0dd94324cf
serv-inc <serv-inc@users.noreply.github.com>
2018-12-24 04:40:50 +0100

    makefile failed more (nullptr, event_tag_map.cpp)

--- liblog.mk
+++ liblog.mk
@@ -2,13 +2,14 @@ NAME = liblog
 SOURCES = log_event_list.c log_event_write.c logger_write.c \
           config_write.c logger_name.c logger_lock.c \
           fake_log_device.c fake_writer.c \
-          event_tag_map.c
+          event_tag_map.cpp
 SOURCES := $(foreach source, $(SOURCES), liblog/$(source))
-CFLAGS += -fvisibility=hidden
+CFLAGS += -fvisibility=hidden -fPIC
 CPPFLAGS += -Iinclude \
             -DLIBLOG_LOG_TAG=1005 \
             -DFAKE_LOG_DEVICE=1 \
-            -DSNET_EVENT_LOG_TAG=1397638484
+            -DSNET_EVENT_LOG_TAG=1397638484 \
+            -std=gnu++11
 LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 -lpthread
 
 build: $(SOURCES)
