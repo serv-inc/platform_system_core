1c76155cc9decc8bcd2247ddc88e9c720440f048
serv-inc <serv-inc@users.noreply.github.com>
2019-01-15 04:57:45 +0100

    off_t is also 64 bit on OpenBSD
     (see ports/.../adb/patches/patch-libsparse_defs_h)

--- base/include/android-base/off64_t.h
+++ base/include/android-base/off64_t.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(__OpenBSD__)
 /** Mac OS has always had a 64-bit off_t, so it doesn't have off64_t. */
 typedef off_t off64_t;
 #endif
